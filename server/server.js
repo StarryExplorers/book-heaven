76879f485907d3fb215cb35cdad310d662685cf1
StarryExplorers <starryexplorers@gmail.com>
2025-05-19 19:13:58 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => res.send('Book Heaven API Running'));
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
+  .catch(err => console.error('MongoDB connection failed:', err));
