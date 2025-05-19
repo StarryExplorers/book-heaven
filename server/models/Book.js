eefbd5e9fe462f0b137ab27ab4b60c194cc8366a
StarryExplorers <starryexplorers@gmail.com>
2025-05-19 19:18:27 +0500

    Update Book.js

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
 
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  genre: { type: String, required: true },
+  description: { type: String },
+  publishedDate: { type: Date },
+  bookFile: { type: String, required: true },
+  coverImage: { type: String }
+});
+
+module.exports = mongoose.model('Book', bookSchema);
